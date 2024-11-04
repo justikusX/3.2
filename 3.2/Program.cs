2ce160a3421f61c730be34a5eda3caf12ef8a760
Илья <@DESKTOP-VPI29MR>
2024-11-04 13:02:38 +0200

    Добавьте файлы проекта.

---
+++ 3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+try
+{
+    Console.Write("Input x: ");
+    int x = int.Parse(Console.ReadLine());
+    Console.Write("Input n: ");
+    int n = int.Parse(Console.ReadLine());
+    int k = 1;
+    int F = 1;
+    double P = 1;
+    for(int i = 1; i <= k ; i++ )
+    {
+        F = F * i;
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Factorial of {k} is {F}");
+    for (int i = 1;i <= n ; i++)
+    {
+        if (x != -3) 
+        {
+            P *= Math.Pow(-1,k) * Math.Pow((x + 3),2)/F;
+        }
+        Console.WriteLine($"Q = {P}");
+    }
+}
+catch
+{
+    Console.WriteLine("Erorr");
+}
